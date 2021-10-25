fdb3443346156412bafb11fbc46f476fdda74fe3
Cezary Piątek <cezarypiatek@live.com>
2021-10-25 19:27:25 +0200

    Add option for overriding how diff result is presented

---
+++ src/ApprovalTestsExtensions/IJsonDiffFormatter.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SmartAnalyzers.ApprovalTestsExtensions
+{
+    /// <summary>
+    ///     This component allows for customizing JSON diff before verifying it with Approval
+    /// </summary>
+    public interface IJsonDiffFormatter
+    {
+        string Format(JToken diff);
+    }
+}
