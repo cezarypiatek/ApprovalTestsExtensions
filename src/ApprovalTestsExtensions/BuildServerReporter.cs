2cbe1837b88d1fca910dc40b8fdc9bf925648d09
Cezary Piątek <cezarypiatek@live.com>
2021-10-25 19:29:27 +0200

    BREAKING CHANGES:  Add default reporters and BuildServerReport wrapper to solved the issue with FrameworkAssertReporter which is a part of DefaultFrontedLoad and it's not working correctly in async lambas

---
+++ src/ApprovalTestsExtensions/BuildServerReporter.cs
@@ -0,0 +1,20 @@
+﻿using ApprovalTests.Core;
+using ApprovalTests.Reporters;
+
+namespace SmartAnalyzers.ApprovalTestsExtensions
+{
+    public class BuildServerReporter: IEnvironmentAwareReporter
+    {
+        private readonly IApprovalFailureReporter _failureReporter;
+        private bool? _isBuildServerEnvironment;
+
+        public BuildServerReporter(IApprovalFailureReporter failureReporter)
+        {
+            _failureReporter = failureReporter;
+        }
+
+        public void Report(string approved, string received) => _failureReporter.Report(approved, received);
+
+        public bool IsWorkingInThisEnvironment(string forFile) => _isBuildServerEnvironment ??= DefaultFrontLoaderReporter.INSTANCE.IsWorkingInThisEnvironment(forFile);
+    }
+}
