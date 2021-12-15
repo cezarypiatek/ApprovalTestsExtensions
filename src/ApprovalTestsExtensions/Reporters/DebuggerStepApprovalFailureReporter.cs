b96a799af231cf40c244d7dc732dd4ed54aaa365
Cezary Piątek <cezarypiatek@live.com>
2021-12-15 19:01:57 +0100

    Add failure reporter which allows for approving all failed snapshot step by step without a need to re-run tests

---
+++ src/ApprovalTestsExtensions/Reporters/DebuggerStepApprovalFailureReporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using ApprovalTests.Core;
+
+namespace SmartAnalyzers.ApprovalTestsExtensions.Reporters
+{
+    /// <summary>
+    ///     Breaks after detecting every failed snapshot and allows to continue after approving
+    /// </summary>
+    public class DebuggerStepApprovalFailureReporter : IReporterWithApprovalPower
+    {
+        private readonly IApprovalFailureReporter _failureReporter;
+
+        public DebuggerStepApprovalFailureReporter(IApprovalFailureReporter failureReporter)
+        {
+            _failureReporter = failureReporter;
+        }
+
+        public bool ApprovedWhenReported() => Debugger.IsAttached;
+
+        public void Report(string approved, string received)
+        {
+            _failureReporter.Report(approved, received);
+            Debugger.Break();
+        }
+    }
+}
